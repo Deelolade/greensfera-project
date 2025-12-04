ea0f073af0f8c27e91f8585463758d2b4a18200d
Deelolade <oluwanisholahabeeb13@icloud.com>
2025-12-04 17:59:25 +0100

    feat:initialized router-dom to all routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,25 @@ import { useState } from 'react';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import About from './components/About';
-import Projects from './components/Projects';
+// import Projects from './components/Projects';
 import Impact from './components/Impact';
-import YouthHub from './components/YouthHub';
+// import YouthHub from './components/YouthHub';
 import Gallery from './components/Gallery';
 import GetInvolved from './components/GetInvolved';
 import Footer from './components/Footer';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import WhoWeAre from './pages/WhoWeAre';
+import Projects from './pages/Projects';
+import YouthHub from './pages/YouthHub';
+import Contact from './pages/Contact';
 
 function App() {
   const [currentSection, setCurrentSection] = useState('home');
 
   return (
-    <div className="min-h-screen bg-white">
+    <>
+    {/* <div className="min-h-screen bg-white">
       <Header currentSection={currentSection} setCurrentSection={setCurrentSection} />
       {currentSection === 'home' && (
         <>
@@ -31,7 +38,15 @@ function App() {
       {currentSection === 'youth-hub' && <YouthHub setCurrentSection={setCurrentSection} full />}
       {currentSection === 'contact' && <GetInvolved setCurrentSection={setCurrentSection} full />}
       <Footer setCurrentSection={setCurrentSection} />
-    </div>
+    </div> */}
+    <Routes>
+      <Route index element={<Home/>}/>
+      <Route path='who-we-are' element={<WhoWeAre/>}/>
+      <Route path='projects' element={<Projects/>}/>
+      <Route path='youth-hub' element={<YouthHub/>}/>
+      <Route path='contact-us' element={<Contact/>}/>
+    </Routes>
+    </>
   );
 }
 
